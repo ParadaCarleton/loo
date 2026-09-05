e12b4562f6ba9763b2cb352d2d566e9717efe293
VisruthSK <public@visruth.com>
2026-09-05 13:06:01 -0700

    Few renames and some small R things

--- R/loo_difference_plot.R
+++ R/loo_difference_plot.R
@@ -8,7 +8,6 @@
 #' @param psis_object_1,psis_object_2 If using loo version 2.0.0 or greater,
 #' an object returned by the `[loo::psis()]` function (or by the
 #' `[loo::loo()]` function with argument `save_psis` set to `TRUE`).
-#' @param ... Currently unused.
 #' @param group A grouping variable (a vector or factor) the same length
 #' as `y`. Each value in group is interpreted as the group level pertaining
 #' to the corresponding value of `y`. If `FALSE`, ignored.
@@ -24,30 +23,30 @@
 #' arbitrary index. Plotting by index can be useful when categories have
 #' very different sample sizes.
 #'
-#'
 #' @template return-ggplot
 #' @template reference-vis-paper
+#'
 #'
 #' @examples
 #' cbPalette <- c("#636363", "#E69F00", "#56B4E9", "#009E73",
 #'                "#F0E442", "#0072B2","#CC79A7")
 #'
 #' # Plot using groups from WHO
-#' plot_loo_dif(factor(GM@data$super_region_name), loo3, loo2,
+#' plot_loo_difference(factor(GM@data$super_region_name), loo3, loo2,
 #'              group = GM@data$super_region_name, alpha = 0.5,
 #'              jitter = c(0.45, 0.2)
 #'              ) +
 #'              xlab("Region") + scale_colour_manual(values=cbPalette)
 #'
 #' # Plot using groups identified with clustering
-#' plot_loo_dif(factor(GM@data$cluster_region), loo3, loo2,
+#' plot_loo_difference(factor(GM@data$cluster_region), loo3, loo2,
 #'              group = GM@data$super_region_name, alpha = 0.5,
 #'              jitter = c(0.45, 0.2)
 #'              ) +
 #'              xlab("Cluster Group") + scale_colour_manual(values=cbPalette)
 #'
 #' # Plot using an index variable to reduce crowding
-#' plot_loo_dif(1:2980, loo3, loo2, group = GM@data$super_region_name,
+#' plot_loo_difference(1:2980, loo3, loo2, group = GM@data$super_region_name,
 #'              alpha = 0.5, sort_by_group = TRUE
 #'              ) +
 #'              xlab("Index") + scale_colour_manual(values=cbPalette)
@@ -77,18 +76,19 @@
 #' coLoo <- loo(iqFit, save_psis = TRUE)
 #' iqLoo <- loo(coFit, save_psis = TRUE)
 #'
-#' plot_loo_dif(kidiq$mom_iq, coLoo, iqLoo, group = kidiq$mom_hs,
+#' plot_loo_difference(kidiq$mom_iq, coLoo, iqLoo, group = kidiq$mom_hs,
 #'              alpha = 0.5, jitter = c(0.1, 0.1)
 #'              ) +
 #'   ggplot2::geom_smooth() +
 #'   ggplot2::xlab("IQ of Mother") +
 #'   ggplot2::scale_colour_manual(values=cbPalette)
-plot_loo_dif <-
+#'
+#' @export
+plot_loo_difference <-
   function(
     y,
     psis_object_1,
     psis_object_2,
-    ...,
     group = NULL,
     outlier_thresh = NULL,
     size = 1,
@@ -98,23 +98,24 @@ plot_loo_dif <-
   ) {
     if (!requireNamespace("ggplot2", quietly = TRUE)) {
       stop(
-        "Please install 'ggplot2' to use `plot_loo_dif()`.",
+        "Please install 'ggplot2' to use `plot_loo_difference()`.",
         call. = FALSE
       )
     }
-    {}
 
     # Adding a 0 at the end lets users provide a single number as input.
     # In this case, only horizontal jitter is applied.
-    jitter <- c(jitter, 0)
+    if (length(jitter) == 1L) {
+      jitter <- c(jitter, 0)
+    }
 
-    elpdDif <- psis_object_1$pointwise[, "elpd_loo"] -
+    elpd_diff <- psis_object_1$pointwise[, "elpd_loo"] -
       psis_object_2$pointwise[, "elpd_loo"]
 
     if (sort_by_group) {
       if (identical(group, NULL) || !identical(y, seq_along(y))) {
         stop(
-          "ERROR: sort_by_group should only be used for grouping categorical 
+          "sort_by_group should only be used for grouping categorical 
              variables, then plotting them with an arbitrary index. You can
              create such an index using `1:length(data)`.
              "
@@ -122,17 +123,19 @@ plot_loo_dif <-
       }
 
       ordering <- order(group)
-      elpdDif <- elpdDif[ordering]
+      elpd_diff <- elpd_diff[ordering]
       group <- group[ordering]
     }
 
-    plot <- ggplot2::ggplot(mapping = aes(y, elpdDif)) +
+    plot <- ggplot2::ggplot(mapping = ggplot2::aes(y, elpd_diff)) +
       ggplot2::geom_hline(yintercept = 0) +
-      ggplot2::xlab(ifelse(sort_by_group, "y", "Index")) +
-      ggplot2::ylab(expression(ELPD[i][1] - ELPD[i][2])) +
-      ggplot2::labs(color = "Groups")
+      ggplot2::labs(
+        x = if (sort_by_group) "y" else "Index",
+        y = expression(ELPD[i][1] - ELPD[i][2]),
+        color = "Groups"
+      )
 
-    if (identical(group, FALSE)) {
+    if (is.null(group)) {
       # Don't color by group if no groups are passed
       plot <- plot +
         ggplot2::geom_jitter(
@@ -145,7 +148,7 @@ plot_loo_dif <-
       # If group is passed, use color
       plot <- plot +
         ggplot2::geom_jitter(
-          aes(color = factor(group)),
+          ggplot2::aes(color = factor(group)),
           width = jitter[1],
           height = jitter[2],
           alpha = alpha,
@@ -155,7 +158,7 @@ plot_loo_dif <-
 
     if (!identical(outlier_thresh, NULL)) {
       # Flag outliers
-      is_outlier <- elpdDif > outlier_thresh
+      is_outlier <- elpd_diff > outlier_thresh
       index <- seq_along(y)
       outlier_labs <- index[is_outlier]
 
@@ -163,7 +166,7 @@ plot_loo_dif <-
         ggplot2::annotate(
           "text",
           x = y[is_outlier],
-          y = elpdDif[outlier_labs],
+          y = elpd_diff[outlier_labs],
           label = outlier_labs,
           size = 4
         )
